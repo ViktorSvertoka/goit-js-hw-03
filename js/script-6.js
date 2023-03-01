16d04565b0bc9ec9bdf2b4ff81d27581d9d6d63b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 01:59:55 +0200

    Update script-6.js

--- js/script-6.js
+++ js/script-6.js
@@ -1,31 +1,3 @@
-// # Завдання 6
-
-// Напиши функцію `calculateTotalPrice(allProdcuts, productName)`, яка отримує
-// масив об'єктів та ім'я продукту (значення властивості `name`). Повертає загальну
-// вартість продукту (ціна \* кількість).
-
-// Викличи функції для перевірки працездатності твоєї реалізації.
-
-// ```js
-// const products = [
-//   { name: "Радар", price: 1300, quantity: 4 },
-//   { name: "Сканер", price: 2700, quantity: 3 },
-//   { name: "Дроїд", price: 400, quantity: 7 },
-//   { name: "Захоплення", price: 1200, quantity: 2 },
-// ];
-
-// const calculateTotalPrice = function (allProdcuts, productName) {
-//   // твій код
-// };
-
-// /*
-//  * Викличи функції для перевірки працездатності твоєї реалізації.
-//  */
-// console.log(calculateTotalPrice(products, "Радар")); // 5200
-
-// console.log(calculateTotalPrice(products, "Дроїд")); // 2800
-// ```
-
 const products = [
   { name: 'Радар', price: 1300, quantity: 4 },
   { name: 'Сканер', price: 2700, quantity: 3 },
