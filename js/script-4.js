ec18be4148e320f88b0f769355e18bd7e7a52795
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 01:36:16 +0200

    Update script-4.js

--- js/script-4.js
+++ js/script-4.js
@@ -1,36 +1,3 @@
-// # Завдання 4
-
-// Напиши функцію `countTotalSalary(employees)` приймаючу об'єкт зарплат. Функція
-// рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта,
-// переданого в функцію, має вигляд `"ім'я":"зарплата"`.
-
-// ```js
-// const countTotalSalary = function (employees) {
-//   // твій код
-// };
-
-// /*
-//  * Викличи функції для перевірки працездатності твоєї реалізації.
-//  */
-// console.log(countTotalSalary({})); // 0
-
-// console.log(
-//   countTotalSalary({
-//     mango: 100,
-//     poly: 150,
-//     alfred: 80,
-//   })
-// ); // 330
-
-// console.log(
-//   countTotalSalary({
-//     kiwi: 200,
-//     lux: 50,
-//     chelsy: 150,
-//   })
-// ); // 400
-// ```
-
 const countTotalSalary = function (employees) {
   const values = Object.values(employees);
 
