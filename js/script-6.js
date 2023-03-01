44f1bf59c3f66541208611f7fe0f39b09f3e3665
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 01:59:13 +0200

    Feat(js): added script to task 6 - task done

--- js/script-6.js
+++ js/script-6.js
@@ -1,13 +1,52 @@
-let input;
-let total = 0;
+// # Завдання 6
 
-while (input !== null) {
-  input = prompt('Введіть число');
+// Напиши функцію `calculateTotalPrice(allProdcuts, productName)`, яка отримує
+// масив об'єктів та ім'я продукту (значення властивості `name`). Повертає загальну
+// вартість продукту (ціна \* кількість).
 
-  if (Number.isNaN(Number(input))) {
-    alert('Введенно не число, спробуйте ще раз');
-  } else {
-    total = total + Number(input);
+// Викличи функції для перевірки працездатності твоєї реалізації.
+
+// ```js
+// const products = [
+//   { name: "Радар", price: 1300, quantity: 4 },
+//   { name: "Сканер", price: 2700, quantity: 3 },
+//   { name: "Дроїд", price: 400, quantity: 7 },
+//   { name: "Захоплення", price: 1200, quantity: 2 },
+// ];
+
+// const calculateTotalPrice = function (allProdcuts, productName) {
+//   // твій код
+// };
+
+// /*
+//  * Викличи функції для перевірки працездатності твоєї реалізації.
+//  */
+// console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+// console.log(calculateTotalPrice(products, "Дроїд")); // 2800
+// ```
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроїд', price: 400, quantity: 7 },
+  { name: 'Захоплення', price: 1200, quantity: 2 },
+];
+
+console.table(products);
+
+const calculateTotalPrice = function (allProducts, productName) {
+  let totalPrice = 0;
+
+  for (let product of allProducts) {
+    if (productName === product.name) {
+      totalPrice = product.price * product.quantity;
+    }
   }
-}
-alert(`Загальна сумма чисел ${total}`);
+
+  return totalPrice;
+};
+
+console.log(calculateTotalPrice(products, 'Радар'));
+
+console.log(calculateTotalPrice(products, 'Дроїд'));
